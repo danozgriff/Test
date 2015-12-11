400b24d728db2c92f821e57c4b7a1b2f092580fc
danozgriff <dane.griff@gmail.com>
2015-12-11 22:38:03 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,3 +2,5 @@ import scraperwiki
 import lxml.html
 html = scraperwiki.scrape("http://shareprices.com/ftseallshare")
 root = lxml.html.fromstring(html)
+for el in root.cssselect("div.featured a"):           
+    print el
