1e4c394e4419376edaa6a84cee01d5792f0b8c87
danozgriff <dane.griff@gmail.com>
2015-12-11 23:06:05 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -14,10 +14,10 @@ url = 'http://shareprices.com/ftseallshare'
 
 #data = scraperwiki.scrape("http://www.asx.com.au/asx/research/ASXListedCompanies.csv")
 #url2 = 'https://www.aussiebulls.com/SignalPage.aspx?lang=en&Ticker=WOW.AX'
-    br = mechanize.Browser()
+br = mechanize.Browser()
 
     # sometimes the server is sensitive to this information
-    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
 #####reader = csv.DictReader(data.splitlines()[2:10])
 
@@ -53,17 +53,17 @@ url = 'http://shareprices.com/ftseallshare'
 #####for record in reader:
 #####        print record
 
-    response = br.open(url)
+response = br.open(url)
 
-    for pagenum in range(1):
-        html = response.read()
+for pagenum in range(1):
+    html = response.read()
     
 #comp = re.search(r'MainContent_CompanyTicker(\w{3,}\.AX)span', html).group(0)
 #ASX_Code = '3PL.AX'
   
 #print re.findall(r'MainContent_signalpagehistory_PatternHistory24((.)+)\<\\table\>', html) 
-        test1 = re.search(r'Day\'s Volume(.*?)>(.*?)<(.*?)endcol', html)
-        print test1
+    test1 = re.search(r'Day\'s Volume(.*?)>(.*?)<(.*?)endcol', html)
+    print test1
 
  #       if test1:
    #         test1 = test1.group(0)
