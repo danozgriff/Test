3e5ed9ccae1f3962bd56b5c81fb8d2b53098660c
danozgriff <dane.griff@gmail.com>
2015-12-11 22:44:01 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,20 @@
-import scraperwiki           
+import scraperwiki
+html = scraperwiki.scrape('http://shareprices.com/ftseallshare')
+print "Click on the ...more link to see the whole page"
+print html
+
+# -----------------------------------------------------------------------------
+# 1. Parse the raw HTML to get the interesting bits - the part inside <td> tags.
+# -- UNCOMMENT THE 6 LINES BELOW (i.e. delete the # at the start of the lines)
+# -- CLICK THE 'RUN' BUTTON BELOW
+# Check the 'Console' tab again, and you'll see how we're extracting 
+# the HTML that was inside <td></td> tags.
+# We use lxml, which is a Python library especially for parsing html.
+# -----------------------------------------------------------------------------
+
 import lxml.html
-html = scraperwiki.scrape("http://shareprices.com/ftseallshare")
-root = lxml.html.fromstring(html)
-for el in root.cssselect("div.featured a"):           
-    print el
+root = lxml.html.fromstring(html) # turn our HTML into an lxml object
+tds = root.cssselect('td') # get all the <td> tags
+for td in tds:
+    print lxml.html.tostring(td) # the full HTML tag
+    print td.text                # just the text inside the HTML tag
