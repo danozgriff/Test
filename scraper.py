462f2b25190cd8ed3bd9b39b6d69999674fa2bb0
danozgriff <dane.griff@gmail.com>
2015-12-14 21:44:02 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -62,7 +62,7 @@ for pagenum in range(1):
 #ASX_Code = '3PL.AX'
   
 #print re.findall(r'MainContent_signalpagehistory_PatternHistory24((.)+)\<\\table\>', html) 
-    test1 = re.findall(r'Day\'s Volume(.*?)>(.*?)<(.*?)endcol', html)
+    test1 = re.findall(r'Day\'s Volume(.*)>(.*)<(.*)endcol', html)
     print test1
 
  #       if test1:
