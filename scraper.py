384ae55a954e04d2e140a408e988266f8e2178b1
danozgriff <dane.griff@gmail.com>
2016-02-18 23:02:16 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -66,10 +66,9 @@ for pagenum in range(1):
     #test1 = re.findall(r'Day\'s Volume(.*)>(.*)<(.*)endcol', html)
     #test1 = re.findall('(Day\'s Volume)(.*?)>(.*?)<(.*?)(endcol)', html)
     test1 = re.search(r'Day\'s Volume(.*?)<br \/><\/div>', html).group()
-    #for match in re.findall(r'(\">|\'>)(.*?)<\/', str(test1.replace(" ", "")).replace("><", "")):
-    test2 = re.findall(r'(\">|\'>)(.*?)<\/', str(test1.replace(" ", "")).replace("><", ""))
+    for match in re.findall(r'(\">|\'>)(.*?)<\/', str(test1.replace(" ", "")).replace("><", "")):
         #print re.search('', (.*?)', match).group()
-    print re.search('"'>"(.*?)', test2).group(0)
+        print re.search(r'(\"\'>\"|\'\">\')(.*?)', match).group(0)
         #print re.search(r'', (\.*\?)', match)
        # ("'>", 'III') 
 # ('">', '3iGroup')
