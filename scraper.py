feb9766eb49e02743754957f992a0240a299ecc5
danozgriff <dane.griff@gmail.com>
2016-02-18 23:28:38 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -68,11 +68,9 @@ for pagenum in range(1):
     test1 = re.search(r'Day\'s Volume(.*?)<br \/><\/div>', html).group()
     #for match in re.findall(r'(\">|\'>)(.*?)<\/', str(test1.replace(" ", "")).replace("><", "")):
     tuples = re.findall(r'(\">|\'>)(.*?)<\/', str(test1.replace(" ", "")).replace("><", ""))
-    for tuple in tuples:
-        print tuple[0]
-        print "buffer1" ## username
+    for tuple in range(1,20):
         print tuple[1]
-        print "buffer2"## host
+
         #print re.search('', (.*?)', match).group()
         #print re.search(r'(\">|\'>)(.*?)', match).group(1)
         #print re.search(r'', (\.*\?)', match)
