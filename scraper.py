10428acbe40628e02576f1ebcd6512ae596f1c2c
danozgriff <dane.griff@gmail.com>
2017-02-20 10:11:17 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -345,6 +345,7 @@ def ScrapeSignalHistory(runno):
         
         #tidm = str(x)[3:-2]
         tidm = str(x)
+        print tidm
         
         ##siglist = scraperwiki.sqlite.execute("select count(*) from Signal_History where tidm = '%s' and (Signal IN ('SELL',  'SHORT',  'STAY IN CASH',  'STAY SHORT') OR (Signal IN ('BUY, 'STAY LONG') AND ))" % (tidm, d1date))
 
