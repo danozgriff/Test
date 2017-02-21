73ccd5a5eb12adf3c8a3c875f47ed3e14b52f22d
danozgriff <dane.griff@gmail.com>
2017-02-21 15:41:47 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -47,7 +47,7 @@ def ScrapeLivePrices(rerunflag):
     dtnow = datetime.datetime.utcnow()
     #print now
     ftseopen = dtnow.replace(hour=0, minute=1, second=0, microsecond=0)
-    ftseclosed = dtnow.replace(hour=7, minute=34, second=0, microsecond=0)
+    ftseclosed = dtnow.replace(hour=7, minute=45, second=0, microsecond=0)
     wkday = datetime.datetime.today().weekday()
     timetilclose = (ftseclosed - dtnow).total_seconds()
     if timetilclose < 0:
